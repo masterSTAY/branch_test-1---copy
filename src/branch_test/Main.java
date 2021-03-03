abb6450f7431f3746f80aca4c7e4c92b03dedc23
SeoTaeHui <masta95@naver.com>
2021-03-03 20:11:09 +0900

    first

---
+++ src/branch_test/Main.java
@@ -0,0 +1,10 @@
+package branch_test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+           System.out.println("HELLO");
+    }
+
+}
